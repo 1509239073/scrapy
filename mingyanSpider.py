769f82153390c0f1b7ce89eed0692ced9ce9bc9d
taixing <1509239073@qq.com>
2018-12-20 13:13:31 +0800

    名言获取body

--- mingyanSpider.py
+++ mingyanSpider.py
@@ -1,2 +1,18 @@
 import scrapy
-print(1);
+class mingyanSpider(scrapy.Spider):
+    name = "quotes"
+    start_urls = [
+        'http://lab.scrapyd.cn/',
+    ]
+
+    def parse(self, response):
+        for quote in response.css('div.quote'):
+        	# quote = unicode(quote,'UTF-8')
+            yield {
+                '内容': quote.css('span.text::text').extract_first(),
+                '作者': quote.xpath('span/small/text()').extract_first(),
+            }
+
+        next_page = response.css('li.next a::attr("href")').extract_first()
+        if next_page is not None:
+            yield scrapy.Request(next_page, self.parse)
